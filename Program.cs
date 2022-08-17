9366d9f4dfa638fb7263d301b10d05f7a11e64f4
Nick Tse <nicholashtse@gmail.com>
2022-08-16 19:16:17 -0700

    update program.cs file to incorporate ApiCall and RestSharp

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
 
-namespace ConsoleApiCall
+namespace ApiTest
 {
-    class Program
+  class Program
+  {
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
+      var result = apiCallTask.Result;
+      Console.WriteLine(result);
     }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
 }
